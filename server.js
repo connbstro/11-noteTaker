041951bbec4a4e5dcd83eb10c01b007e93cd82ec
Connor S <constroh@gmail.com>
2023-05-12 11:57:56 -0500

    apiRoutes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,5 +7,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
